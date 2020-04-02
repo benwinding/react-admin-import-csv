6262c96dea2b3563fa94e25f5546b6bae9bd551a
Ben Winding <hello@benwinding.com>
2020-04-02 18:18:43 +1030

    Better test scripts

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,5 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverage: true,
-  coverageReporters: ["json-summary", "text", "lcov"]
+  coverageReporters: ["json-summary","text","lcov"]
 };
