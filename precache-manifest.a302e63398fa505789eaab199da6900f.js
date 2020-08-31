7a77cfb5ef53e21d199dc756bc49905bb5d7c96f
benwinding <benwinding@users.noreply.github.com>
2020-08-31 00:33:55 +0000

    Deploying to gh-pages from  @ ece80b9a103c25b5ec130e5c0ce45900613cbcf5 🚀

---
+++ precache-manifest.a302e63398fa505789eaab199da6900f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae101b97662e3e48557d2463cacc51cb",
+    "url": "/react-admin-import-csv/index.html"
+  },
+  {
+    "revision": "e20f08ee7eebb2f4914f",
+    "url": "/react-admin-import-csv/static/js/2.ec61cb20.chunk.js"
+  },
+  {
+    "revision": "62453f4919dc2d34bf1a623d1d8a56f1",
+    "url": "/react-admin-import-csv/static/js/2.ec61cb20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724b7e65b701a0c1004c",
+    "url": "/react-admin-import-csv/static/js/main.5856267b.chunk.js"
+  },
+  {
+    "revision": "9bfb3322ff1cec7823d3",
+    "url": "/react-admin-import-csv/static/js/runtime-main.dfda65d6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-admin-import-csv/precache-manifest.02b355798f0656b643c70b7ca2ad1fb3.js"
+  "/react-admin-import-csv/precache-manifest.a302e63398fa505789eaab199da6900f.js"
 );
 
 self.addEventListener('message', (event) => {
