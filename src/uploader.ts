c614452cde14c593ce32bf3984e5d20b640be5e9
Ben Winding <hello@benwinding.com>
2020-09-15 16:56:21 +0930

    Implement createMany handling in uploader

--- src/uploader.ts
+++ src/uploader.ts
@@ -1,35 +1,91 @@
-export async function create(dataProvider, resource, values, postCommitCallback = null) {
+import { DataProvider } from "ra-core";
 
-  if(postCommitCallback) {
-    const report = []
-    await Promise
-      .all(values.map((value) => dataProvider
-        .create(resource, { data: value })
-        .then(() => report.push({...value, success: true}))
-        .catch(err => report.push({...value, success: false, err}))))
-        .finally(() => postCommitCallback(report))
-    return;
+export async function create(
+  dataProvider: DataProvider,
+  resource: string,
+  values: any[],
+  postCommitCallback = null,
+  useCreateMany = false,
+  logging = false
+) {
+  if (postCommitCallback) {
+    const report = [];
+    await createInDataProvider(
+      dataProvider,
+      resource,
+      values,
+      useCreateMany,
+      logging
+    )
+      .then(() => report.push({ values, success: true }))
+      .catch((err) => report.push({ values, success: false, err }));
+    return postCommitCallback(report);
   }
-  else {
-    return Promise.all(values.map((value) => dataProvider.create(resource, { data: value })));
-  }
+  return createInDataProvider(
+    dataProvider,
+    resource,
+    values,
+    useCreateMany,
+    logging
+  );
 }
 
-export async function update(dataProvider, resource, values, postCommitCallback = null) {
-  if(postCommitCallback) {
-    const report = []
-
-    await Promise
-      .all(values.map((value) => dataProvider
-        .update(resource, { id: value.id, data: value })
-        .then(() => report.push({...value, success: true}))
-        .catch(err => report.push({...value, success: false, err}))))
-        .finally(() => postCommitCallback(report));
-
-    return;
+export async function update(
+  dataProvider: DataProvider,
+  resource: string,
+  values: any[],
+  postCommitCallback = null,
+  logging = false
+) {
+  if (postCommitCallback) {
+    const report = [];
+    await updateInDataProvider(dataProvider, resource, values, logging)
+      .then(() => report.push({ values, success: true }))
+      .catch((err) => report.push({ values, success: false, err }));
+    return postCommitCallback(report);
   }
-  else {
-    return Promise.all(values.map((value) => dataProvider.update(resource, { id: value.id, data: value })))
-  };
+  return updateInDataProvider(dataProvider, resource, values, logging);
 }
 
+async function createInDataProvider(
+  dataProvider: DataProvider,
+  resource: string,
+  values: any[],
+  useCreateMany: boolean,
+  logging: boolean
+) {
+  if (logging) {
+    console.log("uploader.addInDataProvider", {
+      dataProvider,
+      resource,
+      values,
+      logging,
+      useCreateMany,
+    });
+  }
+  if (useCreateMany) {
+    return dataProvider.createMany(resource, { data: values });
+  }
+  return Promise.all(
+    values.map((value) => dataProvider.create(resource, { data: value }))
+  );
+}
+
+async function updateInDataProvider(
+  dataProvider: DataProvider,
+  resource: string,
+  values: any[],
+  logging: boolean
+) {
+  const ids = values.map((v) => v.id);
+  if (logging) {
+    console.log("uploader.updateInDataProvider", {
+      dataProvider,
+      resource,
+      values,
+      logging,
+      ids,
+    });
+  }
+  return dataProvider.updateMany(resource, { ids: ids, data: values });
+}
