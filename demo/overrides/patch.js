f5df6a68d0e1eb9e4067431fe2e39c9c9b74229f
Ben Winding <hello@benwinding.com>
2020-09-16 22:36:27 +0930

    Better source-maps in demo

---
+++ demo/overrides/patch.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const fs = require("fs");
+
+const destDir = path.join(__dirname, "../../node_modules/react-scripts/config");
+
+console.log(
+  'patching webpack so "react-firebase-admin" is watched for changes'
+);
+
+// fs.copyFileSync(
+//   path.join(__dirname, "./webpack.config.prod.js"),
+//   path.join(destDir, "./webpack.config.prod.js")
+// );
+fs.copyFileSync(
+  path.join(__dirname, "./webpack.config.js"),
+  path.join(destDir, "./webpack.config.js")
+);
+fs.copyFileSync(
+  path.join(__dirname, "./webpackDevServer.config.js"),
+  path.join(destDir, "./webpackDevServer.config.js")
+);
