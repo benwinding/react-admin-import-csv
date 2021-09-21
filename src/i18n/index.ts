043de913b637960fd2ba02d513f7fcc851684568
Ben Winding <hello@benwinding.com>
2021-09-21 10:53:31 +1000

    Fix barrel file for all i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,6 @@
-export { default as es } from './es';
+export { default as de } from './de';
 export { default as en } from './en';
-export { default as zh } from './zh';
+export { default as es } from './es';
+export { default as fr } from './fr';
 export { default as ru } from './ru';
+export { default as zh } from './zh';
