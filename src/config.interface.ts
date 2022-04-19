6cb2ec86202c41ac70854ffff3c1431e1926ad12
Ben Winding <hello@benwinding.com>
2022-04-19 14:14:11 +1000

    Add help information to Config Type

--- src/config.interface.ts
+++ src/config.interface.ts
@@ -1,16 +1,26 @@
 import { ParseConfig } from "papaparse";
 
 export interface ImportConfig {
+  // Enable logging
   logging?: boolean;
-  parseConfig?: ParseConfig;
-  disableCreateMany?: boolean;
-  disableUpdateMany?: boolean;
+  // Disable the attempt to use "createMany", will instead just use "create" calls
+  disableCreateMany?: boolean,
+  // Disable the attempt to use "updateMany", will instead just use "update" calls
+  disableUpdateMany?: boolean,
+  // Disable "import new" button
   disableImportNew?: boolean;
+  // Disable "import overwrite" button
   disableImportOverwrite?: boolean;
+  // A function to translate the CSV rows on import
   preCommitCallback?: PrecommitCallback;
+  // A function to handle row errors after import
   postCommitCallback?: ErrorCallback;
+  // Transform rows before anything is sent to dataprovider
   transformRows?: (csvRows: any[]) => Promise<any[]>;
+  // Async function to Validate a row, reject the promise if it's not valid
   validateRow?: ValidateRowFunction;
+  // Any option from the "papaparse" library, for all options see: https://www.papaparse.com/docs#config
+  parseConfig?: ParseConfig,
 };
 
 export type PrecommitCallback = (action: "create" | "overwrite", values: any[]) => Promise<any[]>;
